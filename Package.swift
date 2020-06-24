33603e5dbcecd7b26d06bdb4b28ffa4417052b64
Johnny Vega Sosa <johnny.vega@me.com>
2020-06-24 13:12:59 -0500

    Swift Package Update

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Categorization",
     platforms: [
-        .iOS(.v12),
-        .macOS(.v10_13),
+        .iOS(.v13),
+        .macOS(.v10_14),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
